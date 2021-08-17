2f5da0017f59003f02edeeee7b1e6daa2c85dd41
tomas-correa <tomascorrea93@gmail.com>
2021-08-16 21:49:21 -0300

    Creando Repositorio

---
+++ clase4.js
@@ -0,0 +1,26 @@
+let temperatura = 25;
+let lluvia = false;
+let saldoSube = 300;
+let valorBoleto = 22;
+let nombreRecepcionista = "Anastasia";
+let horaActual = 8;
+let horaIngresoTrabajo = 9;
+let horaFinTrabajo = 21;
+let cantidadCasosResueltos = 2;
+let nombreJefe = "Ricardo";
+
+console.log(
+  `Hoy cuando salí de mi casa hacían ${temperatura} grados, pero llovia? ${lluvia}. Me quedaban ${saldoSube} en la tarjeta. Me recibio en la oficina muy simpatica ${nombreRecepcionista}.`
+);
+
+let cajonRemeras = "pantalones"
+let cajonPantalones = "remeras"
+
+function intercambiarCajones(){
+        let cajonRemeras = "remeras"
+        let cajonPantalones = "pantalones"    
+console.log (`En mi cajon de remeras tengo ${cajonRemeras} y en el de pantalones tengo ${cajonPantalones}`)
+}
+console.log(cajonRemeras)
+console.log(cajonPantalones)
+intercambiarCajones()
---
+++ probando.js
@@ -0,0 +1,7 @@
+
+let edad = 2
+let perro = false
+let nombre = "juan"
+
+console.log(`mi nombre es ${edad} tengo ${nombre} años`)
+console.log('PROBANDO NODE JS')
